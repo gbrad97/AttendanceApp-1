1bd2d26ab61248b6e92ab161e97a6b35d3fac320
Grace Braden <gracebraden97@gmail.com>
2018-01-22 11:22:31 -0600

    Read user's name and output as a welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\n\nHi, AttendanceApp!\n");
-        System.out.println("Welcome to Attendance App!");
+        //System.out.println("\n\nHi, AttendanceApp!\n");
+        //System.out.println("Welcome to Attendance App!");
+
+        outputWelcomeMsg();
+    }
+
+    public static void outputWelcomeMsg() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.next();
+        System.out.println("Hello " + name + "! Welcome to Attendance App!");
     }
 }
