cf05b803d62729e2c518b20ecd0c0f73c817c18a
Grace Braden <gracebraden97@gmail.com>
2018-01-22 11:15:30 -0600

    Add a welcome message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHi, AttendanceApp!\n");
+        System.out.println("Welcome to Attendance App!");
     }
 }
