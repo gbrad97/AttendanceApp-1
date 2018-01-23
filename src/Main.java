0fb884061032624017a66a7c3b09c8c08416591e
Grace Braden <gracebraden97@gmail.com>
2018-01-22 19:47:54 -0600

    Generates a list of absences, given a number of students and the max number of absences a student can have, and outputs this list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,9 @@ public class Main {
         //System.out.println("Welcome to Attendance App!");
 
         outputWelcomeMsg();
+
+        // Outputs a list of absences for 10 students with a max num of 15 absences
+        outputList(generateListOfAbsences(10, 15));
     }
 
     public static void outputWelcomeMsg() {
@@ -14,5 +19,22 @@ public class Main {
         System.out.print("Enter your name: ");
         String name = scan.next();
         System.out.println("Hello " + name + "! Welcome to Attendance App!");
+    }
+
+    public static ArrayList<Integer> generateListOfAbsences(int numOfStudents,
+                                                            int maxNumOfAbsences) {
+        ArrayList<Integer> listOfAbsences = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < numOfStudents; i++) {
+            int absences = rand.nextInt(maxNumOfAbsences + 1);
+            listOfAbsences.add(absences);
+        }
+        return listOfAbsences;
+    }
+
+    public static void outputList(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Student " + i + ": " + list.get(i));
+        }
     }
 }
